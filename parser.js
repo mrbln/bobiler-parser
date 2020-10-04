a1413776de378fea2dac36631bae3fb8380415e3
mrbln <belen.emre@gmail.com>
2020-10-04 21:41:36 +0300

    Change html codes to plaint text

--- parser.js
+++ parser.js
@@ -16,9 +16,26 @@ Http.onreadystatechange = (e) => {
         let headerList = [];
 
         for (let item of list) {
-            headerList.push(item.getElementsByClassName("uk-margin-remove")[0].innerHTML.trim());
+            headerList.push(decodeEntities(item.getElementsByClassName("uk-margin-remove")[0].innerHTML.trim()));
         }
 
         console.log(headerList);
     }
+}
+
+function decodeEntities(encodedString) {
+    var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
+    var translate = {
+        "nbsp":" ",
+        "amp" : "&",
+        "quot": "\"",
+        "lt"  : "<",
+        "gt"  : ">"
+    };
+    return encodedString.replace(translate_re, function(match, entity) {
+        return translate[entity];
+    }).replace(/&#(\d+);/gi, function(match, numStr) {
+        var num = parseInt(numStr, 10);
+        return String.fromCharCode(num);
+    });
 }
